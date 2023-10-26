7e4ba6707c748d8ed4879b9a06cd745cb740a3e8
khanhndxd <khanhprox25x@gmail.com>
2023-10-26 16:34:42 +0700

    Add method in RepairOrder controller to get all the orders

---
+++ Controllers/RepairOrderController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using repair_management_backend.Repositories.RepairOrderRepo;
+
+namespace repair_management_backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RepairOrderController : ControllerBase
+    {
+        private readonly DataContext _dataContext;
+        private readonly IRepairOrderRepository _repairOrderRepository;
+        public RepairOrderController(DataContext dataContext, IRepairOrderRepository repairOrderRepository)
+        {
+            _dataContext = dataContext;
+            _repairOrderRepository = repairOrderRepository;
+        }
+        [HttpGet("GetAll")]
+        public async Task<ActionResult<ServiceResponse<List<RepairOrder>>>> Get()
+        {
+            return Ok(await _repairOrderRepository.GetAll());
+        }
+    }
+}
