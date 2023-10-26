0d92b60b8975ef012979953e019732ee2fc4807e
khanhndxd <khanhprox25x@gmail.com>
2023-10-26 16:31:59 +0700

    Add RepairOrder repository

---
+++ Repositories/RepairOrderRepo/RepairOrderRepository.cs
@@ -0,0 +1,17 @@
+﻿namespace repair_management_backend.Repositories.RepairOrderRepo
+{
+    public class RepairOrderRepository : IRepairOrderRepository
+    {
+        private readonly DataContext _dataContext;
+        public RepairOrderRepository(DataContext dataContext)
+        {
+            _dataContext = dataContext;
+        }
+        public async Task<ServiceResponse<List<RepairOrder>>> GetAll()
+        {
+            var serviceResponse = new ServiceResponse<List<RepairOrder>>();
+            serviceResponse.Data = await _dataContext.RepairOrders.ToListAsync();
+            return serviceResponse;
+        }
+    }
+}
