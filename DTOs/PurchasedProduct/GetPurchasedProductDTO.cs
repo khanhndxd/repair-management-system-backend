14de1d531feeed6c0a86d7b7a5f997cf350058f4
khanhndxd <khanhprox25x@gmail.com>
2023-11-01 21:53:19 +0700

    Add more required property

--- DTOs/PurchasedProduct/GetPurchasedProductDTO.cs
+++ DTOs/PurchasedProduct/GetPurchasedProductDTO.cs
@@ -10,6 +10,7 @@ namespace repair_management_backend.DTOs.PurchasedProduct
         [MaxLength(255)]
         public string ProductSerial { get; set; }
         public double Price { get; set; }
+        public DateTime WarrantyPeriod { get; set; }
         public virtual ICollection<RepairProduct> RepairProducts { get; set; }
     }
 }
