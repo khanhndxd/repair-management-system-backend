aecd4a1d6356fb5d6ed569e08a5d02ca037e7dce
khanhndxd <khanhprox25x@gmail.com>
2023-11-08 23:34:16 +0700

    Add service to update repair order and add repair product

---
+++ Repositories/RepairProductRepo/IRepairProductRepository.cs
@@ -0,0 +1,9 @@
+﻿using repair_management_backend.DTOs.RepairProduct;
+
+namespace repair_management_backend.Repositories.RepairProductRepo
+{
+    public interface IRepairProductRepository
+    {
+        Task<ServiceResponse<string>> AddRepairProduct(List<AddRepairProductDTO> addRepairProductDTO);
+    }
+}
