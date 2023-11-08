48d080618a42052ea155ca4b93a01c96afebd000
khanhndxd <khanhprox25x@gmail.com>
2023-11-08 23:29:15 +0700

    Add repair product controller

---
+++ Controllers/RepairProductController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using repair_management_backend.DTOs.RepairProduct;
+using repair_management_backend.Repositories.RepairProductRepo;
+
+namespace repair_management_backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RepairProductController : ControllerBase
+    {
+        private readonly IRepairProductRepository _repairProductRepository;
+        public RepairProductController(IRepairProductRepository repairProductRepository)
+        {
+            _repairProductRepository = repairProductRepository;
+        }
+        [HttpPost]
+        public async Task<ActionResult<ServiceResponse<string>>> AddRepairProduct([FromBody] List<AddRepairProductDTO> addRepairProductDTO)
+        {
+            var result = await _repairProductRepository.AddRepairProduct(addRepairProductDTO);
+            if (result.Data is null)
+            {
+                return BadRequest(result);
+            }
+            return Ok(result);
+        }
+    }
+}
