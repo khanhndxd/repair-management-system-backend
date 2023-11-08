6ff47457f600b1e30edb791c06ea4f74910eb9d0
khanhndxd <khanhprox25x@gmail.com>
2023-11-08 23:32:46 +0700

    Create DTO to add repair product

---
+++ DTOs/RepairProduct/AddRepairProductDTO.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace repair_management_backend.DTOs.RepairProduct
+{
+    public class AddRepairProductDTO
+    {
+        public int RepairOrderId { get; set; }
+        public int PurchasedProductId { get; set; }
+        [MaxLength(255)]
+        public string Description { get; set; } = string.Empty;
+    }
+}
