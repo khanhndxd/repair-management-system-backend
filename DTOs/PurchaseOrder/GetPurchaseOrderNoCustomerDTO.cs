d8f909f37b0f619652b5b58583c212b7eb22ee6f
khanhndxd <khanhprox25x@gmail.com>
2023-11-09 23:28:11 +0700

    Create GetPurchaseOrderNoCustomerDTO to prevent nesting data in response

---
+++ DTOs/PurchaseOrder/GetPurchaseOrderNoCustomerDTO.cs
@@ -0,0 +1,14 @@
+﻿using repair_management_backend.DTOs.Customer;
+using repair_management_backend.DTOs.PurchasedProduct;
+
+namespace repair_management_backend.DTOs.PurchaseOrder
+{
+    public class GetPurchaseOrderNoCustomerDTO
+    {
+        public int Id { get; set; }
+        public int CustomerId { get; set; }
+        public double Total { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public List<GetPurchasedProductDTO> PurchaseProducts { get; set; }
+    }
+}
