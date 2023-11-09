5825e1ef7e395418b841a4f200a75e85ce72ed7e
khanhndxd <khanhprox25x@gmail.com>
2023-11-09 23:29:35 +0700

    Add UpdateCustomerDTO to using in update customer service

---
+++ DTOs/Customer/UpdateCustomerDTO.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace repair_management_backend.DTOs.Customer
+{
+    public class UpdateCustomerDTO
+    {
+        public int Id { get; set; }
+        [MaxLength(255)]
+        public string Name { get; set; } = string.Empty;
+        [MaxLength(50)]
+        public string Email { get; set; } = string.Empty;
+        [MaxLength(255)]
+        public string Address { get; set; } = string.Empty;
+        [MaxLength(50)]
+        public string Phone { get; set; } = string.Empty;
+        [MaxLength(255)]
+        public string Note { get; set; } = string.Empty;
+    }
+}
