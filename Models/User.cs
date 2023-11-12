e50ca9903e16dc9a418fcd6087e836776a74e3c2
khanhndxd <khanhprox25x@gmail.com>
2023-11-12 20:55:04 +0700

    Add refresh token field and refresh token expiry time field to user table

--- Models/User.cs
+++ Models/User.cs
@@ -4,6 +4,8 @@ namespace repair_management_backend.Models
 {
     public class User : IdentityUser
     {
+        public string? RefreshToken { get; set; }
+        public DateTime RefreshTokenExpiryTime { get; set; }
         public virtual ICollection<RepairOrder> CreatedOrders { get; set; }
         public virtual ICollection<RepairOrder> RepairedOrders { get; set; }
     }
