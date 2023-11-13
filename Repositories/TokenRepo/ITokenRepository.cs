21d3165f0fd4da096054a6e69db673dba0174e0e
khanhndxd <khanhprox25x@gmail.com>
2023-11-13 22:57:40 +0700

    Add token service to generate token, refresh token

---
+++ Repositories/TokenRepo/ITokenRepository.cs
@@ -0,0 +1,12 @@
+﻿using System.Security.Claims;
+
+namespace repair_management_backend.Repositories.TokenRepo
+{
+    public interface ITokenRepository
+    {
+        Task<string> GenerateAccessTokenFromUserName(string username);
+        string GenerateAccessTokenFromClaims(IEnumerable<Claim> claims);
+        string GenerateRefreshToken();
+        ClaimsPrincipal GetPrincipalFromExpiredToken(string token);
+    }
+}
---
+++ Repositories/TokenRepo/TokenRepository.cs
@@ -0,0 +1,90 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using repair_management_backend.Configuration;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace repair_management_backend.Repositories.TokenRepo
+{
+    public class TokenRepository : ITokenRepository
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly JwtConfig _jwtConfig;
+        public TokenRepository(UserManager<IdentityUser> userManager, IOptionsMonitor<JwtConfig> optionsMonitor)
+        {
+            _userManager = userManager;
+            _jwtConfig = optionsMonitor.CurrentValue;
+        }
+        public string GenerateAccessTokenFromClaims(IEnumerable<Claim> claims)
+        {
+            return GenerateJwtToken(claims);
+        }
+        public async Task<string> GenerateAccessTokenFromUserName(string username)
+        {
+            var claims = await GetClaims(username);
+            return GenerateJwtToken(claims);
+        }
+        private string GenerateJwtToken(IEnumerable<Claim> claims)
+        {
+            var jwtTokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.UTF8.GetBytes(_jwtConfig.Secret);
+
+            var securityToken = new JwtSecurityToken(
+                claims: claims,
+                expires: DateTime.UtcNow.AddMinutes(2),
+                signingCredentials: new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+            );
+
+            var jwtToken = jwtTokenHandler.WriteToken(securityToken);
+
+            return jwtToken;
+        }
+        public string GenerateRefreshToken()
+        {
+            var randomNumber = new byte[32];
+            using (var rng = RandomNumberGenerator.Create())
+            {
+                rng.GetBytes(randomNumber);
+                return Convert.ToBase64String(randomNumber);
+            }
+        }
+
+        public ClaimsPrincipal GetPrincipalFromExpiredToken(string token)
+        {
+            var key = Encoding.UTF8.GetBytes(_jwtConfig.Secret);
+            var tokenValidationParameters = new TokenValidationParameters
+            {
+                ValidateAudience = false,
+                ValidateIssuer = false,
+                ValidateIssuerSigningKey = true,
+                IssuerSigningKey = new SymmetricSecurityKey(key),
+                ValidateLifetime = false,
+                RequireExpirationTime = false,
+                ClockSkew = TimeSpan.Zero
+            };
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+            SecurityToken securityToken;
+            var principal = tokenHandler.ValidateToken(token, tokenValidationParameters, out securityToken);
+            var jwtSecurityToken = securityToken as JwtSecurityToken;
+            if (jwtSecurityToken == null || !jwtSecurityToken.Header.Alg.Equals(SecurityAlgorithms.HmacSha256Signature, StringComparison.InvariantCultureIgnoreCase))
+                throw new SecurityTokenException("Invalid token");
+
+            return principal;
+        }
+        private async Task<List<Claim>> GetClaims(string username)
+        {
+            var user = await _userManager.FindByNameAsync(username);
+            var claims = new List<Claim>() { new Claim(ClaimTypes.Name, user.Email) };
+            var roles = await _userManager.GetRolesAsync(user);
+            foreach (var role in roles)
+            {
+                claims.Add(new Claim(ClaimTypes.Role, role));
+            }
+            return claims;
+        }
+    }
+}
