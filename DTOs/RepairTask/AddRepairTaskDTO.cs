c5f64e64634716a25c61cd793808fc9c48f29eac
khanhndxd <khanhprox25x@gmail.com>
2023-11-18 21:10:31 +0700

    Add RepairTask dtos

---
+++ DTOs/RepairTask/AddRepairTaskDTO.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace repair_management_backend.DTOs.RepairTask
+{
+    public class AddRepairTaskDTO
+    {
+        public int RepairOrderId { get; set; }
+        public int TaskId { get; set; }
+        [MaxLength(255)]
+        public string Description { get; set; } = string.Empty;
+    }
+}
---
+++ DTOs/RepairTask/GetRepairTaskDTO.cs
@@ -0,0 +1,17 @@
+﻿using repair_management_backend.DTOs.Task;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace repair_management_backend.DTOs.RepairTask
+{
+    public class GetRepairTaskDTO
+    {
+        public int Id { get; set; }
+        [ForeignKey(nameof(RepairOrder))]
+        public int RepairOrderId { get; set; }
+        [MaxLength(255)]
+        public string Description { get; set; } = string.Empty;
+        [DeleteBehavior(DeleteBehavior.NoAction)]
+        public GetTaskDTO Task { get; set; }
+    }
+}
