4fd2a58ea5975a4e355a846601b7d74a6abb44fc
khanhndxd <khanhprox25x@gmail.com>
2023-11-18 21:09:15 +0700

    Add RepairCustomerProduct dtos

---
+++ DTOs/RepairCustomerProduct/AddRepairCustomerProductDTO.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace repair_management_backend.DTOs.RepairCustomerProduct
+{
+    public class AddRepairCustomerProductDTO
+    {
+        public int RepairOrderId { get; set; }
+        public int CustomerProductId { get; set; }
+        [MaxLength(255)]
+        public string Description { get; set; } = string.Empty;
+    }
+}
---
+++ DTOs/RepairCustomerProduct/GetRepairCustomerProductDTO.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using repair_management_backend.DTOs.CustomerProduct;
+
+namespace repair_management_backend.DTOs.RepairCustomerProduct
+{
+    public class GetRepairCustomerProductDTO
+    {
+        public int Id { get; set; }
+        [ForeignKey(nameof(RepairOrder))]
+        public int RepairOrderId { get; set; }
+        [MaxLength(255)]
+        public string Description { get; set; }
+        [DeleteBehavior(DeleteBehavior.NoAction)]
+        public GetCustomerProductDTO CustomerProduct { get; set; }
+    }
+}
