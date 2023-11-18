d6a8b498b490be64449a42f6b603cfacba2e97e2
khanhndxd <khanhprox25x@gmail.com>
2023-11-18 21:07:07 +0700

    Add customer product dtos

---
+++ DTOs/CustomerProduct/AddCustomerProductDTO.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace repair_management_backend.DTOs.CustomerProduct
+{
+    public class AddCustomerProductDTO
+    {
+        public string Name { get; set; }
+        [MaxLength(255)]
+        public string Note { get; set; }
+        [ForeignKey(nameof(Customer))]
+        public int CustomerId { get; set; }
+    }
+}
---
+++ DTOs/CustomerProduct/GetCustomerProductDTO.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace repair_management_backend.DTOs.CustomerProduct
+{
+    public class GetCustomerProductDTO
+    {
+        public int Id { get; set; }
+        [MaxLength(255)]
+        public string Name { get; set; }
+        [MaxLength(255)]
+        public string Note { get; set; }
+        [ForeignKey(nameof(Customer))]
+        public int CustomerId { get; set; }
+    }
+}
