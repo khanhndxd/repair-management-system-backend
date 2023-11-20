d4ff17d8d2413b07e64445283b81ffc8c63b3bba
khanhndxd <khanhprox25x@gmail.com>
2023-11-20 23:52:25 +0700

    Add more field in Pdf DTO request

--- DTOs/Pdf/PdfGenerationRequest.cs
+++ DTOs/Pdf/PdfGenerationRequest.cs
@@ -2,15 +2,19 @@
 {
     public class PdfGenerationRequest
     {
-        public int repairOrderId { get; set; }
+        public int RepairOrderId { get; set; }
         public string CustomerName { get; set; }
         public string CustomerPhone { get; set; }
         public string CustomerAddress { get; set; }
         public string CustomerEmail { get; set; }
+        public string CreatedBy { get; set; }
         public DateTime CreatedAt { get; set; }
         public DateTime ReceiveAt { get; set; }
+        public string RepairReason { get; set; }
         public List<AccessoryRequest> Accessories { get; set; }
         public List<RepairProductsRequest> RepairProducts { get; set; }
-        public TaskRequest Tasks { get; set; }
+        public List<TaskRequest> Tasks { get; set; }
+        public List<RepairCustomerProductRequest> RepairCustomerProducts { get; set; }
+        public double TotalPrice { get; set; }
     }
 }
---
+++ DTOs/Pdf/RepairCustomerProductRequest.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace repair_management_backend.DTOs.Pdf
+{
+    public class RepairCustomerProductRequest
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
