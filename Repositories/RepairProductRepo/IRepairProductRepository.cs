c9cb42ba69e9d5a4d42a186fc8f62fd2424b0b4c
khanhndxd <khanhprox25x@gmail.com>
2023-11-24 23:26:42 +0700

    Fix typo

--- Repositories/RepairProductRepo/IRepairProductRepository.cs
+++ Repositories/RepairProductRepo/IRepairProductRepository.cs
@@ -4,6 +4,6 @@ namespace repair_management_backend.Repositories.RepairProductRepo
 {
     public interface IRepairProductRepository
     {
-        Task<ServiceResponse<string>> AddRepairProduct(List<AddRepairProductDTO> addRepairProductDTO);
+        Task<ServiceResponse<string>> AddRepairProduct(List<AddRepairProductDTO> addRepairProductDTOs);
     }
 }
--- Repositories/RepairProductRepo/RepairProductRepository.cs
+++ Repositories/RepairProductRepo/RepairProductRepository.cs
@@ -1,6 +1,7 @@
 ﻿using repair_management_backend.Data;
 using repair_management_backend.DTOs.RepairAccessory;
 using repair_management_backend.DTOs.RepairProduct;
+using repair_management_backend.DTOs.RepairTask;
 
 namespace repair_management_backend.Repositories.RepairProductRepo
 {
@@ -11,11 +12,11 @@ namespace repair_management_backend.Repositories.RepairProductRepo
         {
             _dataContext = dataContext;
         }
-        public async Task<ServiceResponse<string>> AddRepairProduct(List<AddRepairProductDTO> addRepairProductDTO)
+        public async Task<ServiceResponse<string>> AddRepairProduct(List<AddRepairProductDTO> addRepairProductDTOs)
         {
             var serviceResponse = new ServiceResponse<string>();
 
-            if (addRepairProductDTO.Count == 0)
+            if (addRepairProductDTOs.Count == 0)
             {
                 serviceResponse.Data = "Thêm thành công sản phẩm vào đơn bảo hành";
                 return serviceResponse;
@@ -25,30 +26,33 @@ namespace repair_management_backend.Repositories.RepairProductRepo
 
             try
             {
-                // Lấy ra danh sách RepairOrderId từ addRepairAccessoryDTO
-                var repairOrderIds = addRepairProductDTO.Select(dto => dto.RepairOrderId).ToList();
+                // Lấy ra danh sách RepairOrderId từ addRepairProductDTO
+                var repairOrderIds = addRepairProductDTOs.Select(dto => dto.RepairOrderId).ToList();
 
                 // Xóa dữ liệu trong RepairProduct có RepairOrderId nằm trong danh sách trước khi thêm mới
                 var repairProductsToDelete = _dataContext.RepairProducts.Where(ra => repairOrderIds.Contains(ra.RepairOrderId));
                 _dataContext.RepairProducts.RemoveRange(repairProductsToDelete);
-
                 await _dataContext.SaveChangesAsync();
 
-                // Thêm dữ liệu mới
-                for (int i = 0; i < addRepairProductDTO.Count; i++)
+                // Kiểm tra xem có sản phẩm nào được thêm không
+                if (addRepairProductDTOs[0].PurchasedProductId != -1 && addRepairProductDTOs.Count >= 1)
                 {
-                    var repairProduct = new RepairProduct
+                    // Thêm dữ liệu mới
+                    for (int i = 0; i < addRepairProductDTOs.Count; i++)
                     {
-                        PurchasedProductId = addRepairProductDTO[i].PurchasedProductId,
-                        RepairOrderId = addRepairProductDTO[i].RepairOrderId,
-                        Description = addRepairProductDTO[i].Description
-                    };
-                    _dataContext.RepairProducts.Add(repairProduct);
+                        var repairProduct = new RepairProduct
+                        {
+                            PurchasedProductId = addRepairProductDTOs[i].PurchasedProductId,
+                            RepairOrderId = addRepairProductDTOs[i].RepairOrderId,
+                            Description = addRepairProductDTOs[i].Description
+                        };
+                        _dataContext.RepairProducts.Add(repairProduct);
 
-                    // cứ thêm 10 sản phẩm vào bộ nhớ thì sẽ lưu vào database
-                    if (i % 10 == 0)
-                    {
-                        await _dataContext.SaveChangesAsync();
+                        // cứ thêm 10 sản phẩm vào bộ nhớ thì sẽ lưu vào database
+                        if (i % 10 == 0)
+                        {
+                            await _dataContext.SaveChangesAsync();
+                        }
                     }
                 }
 
