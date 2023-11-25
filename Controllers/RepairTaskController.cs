eca3a7cab7f93a11b84a4817e3aae15b40913828
khanhndxd <khanhprox25x@gmail.com>
2023-11-25 16:44:16 +0700

    Add RepairTaskController

---
+++ Controllers/RepairTaskController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using repair_management_backend.DTOs.RepairTask;
+using repair_management_backend.Repositories.RepairTask;
+
+namespace repair_management_backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RepairTaskController : ControllerBase
+    {
+        private readonly IRepairTaskRepository _repairTaskRepository;
+        public RepairTaskController(IRepairTaskRepository repairTaskRepository)
+        {
+            _repairTaskRepository = repairTaskRepository;
+        }
+        [HttpPost]
+        public async Task<IActionResult> AddRepairTask([FromBody] List<AddRepairTaskDTO> addRepairTaskDTOs)
+        {
+            var result = await _repairTaskRepository.AddRepairTask(addRepairTaskDTOs);
+            if(result.Success == false)
+            {
+                return BadRequest(result);
+            }
+            return Ok(result);
+        }
+    }
+}
