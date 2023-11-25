018ef863d641730d30526378df315ec0ae57c7e3
khanhndxd <khanhprox25x@gmail.com>
2023-11-25 16:43:02 +0700

    Add Customer Product controller

---
+++ Controllers/CustomerProductController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using repair_management_backend.DTOs.CustomerProduct;
+using repair_management_backend.Repositories.CustomerProductRepo;
+
+namespace repair_management_backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomerProductController : ControllerBase
+    {
+        private readonly ICustomerProductRepository _customerProductRepository;
+        public CustomerProductController(ICustomerProductRepository customerProductRepository)
+        {
+            _customerProductRepository = customerProductRepository;
+        }
+        [HttpPost]
+        public async Task<IActionResult> AddCustomerProduct([FromBody] List<AddCustomerProductDTO> addCustomerProductDTOs)
+        {
+            var result = await _customerProductRepository.AddCustomerProduct(addCustomerProductDTOs);
+            if (result.Success == false)
+            {
+                return BadRequest(result);
+            }
+            return Ok(result);
+        }
+        [HttpGet("Customer/{customerId}")]
+        public async Task<IActionResult> GetCustomerProductByCustomerId(int customerId)
+        {
+            var result = await _customerProductRepository.GetCustomerProductsByCustomerId(customerId);
+            if (result.Success == false)
+            {
+                return NotFound(result);
+            }
+            return Ok(result);
+        }
+    }
+}
