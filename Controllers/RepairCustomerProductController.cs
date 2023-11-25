deb69678e8ae434880964c13a2cf7f6ef05b694d
khanhndxd <khanhprox25x@gmail.com>
2023-11-25 16:47:48 +0700

    Add RepairCustomerProductController

---
+++ Controllers/RepairCustomerProductController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using repair_management_backend.DTOs.RepairCustomerProduct;
+using repair_management_backend.Repositories.RepairCustomerProductRepo;
+
+namespace repair_management_backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RepairCustomerProductController : ControllerBase
+    {
+        private readonly IRepairCustomerProductRepository _repairCustomerProductRepository;
+        public RepairCustomerProductController(IRepairCustomerProductRepository repairCustomerProductRepository)
+        {
+            _repairCustomerProductRepository = repairCustomerProductRepository;
+        }
+        [HttpPost]
+        public async Task<IActionResult> AddRepairCustomerProduct([FromBody] List<AddRepairCustomerProductDTO> addRepairCustomerProductDTOs)
+        {
+            var result = await _repairCustomerProductRepository.AddRepairCustomerProduct(addRepairCustomerProductDTOs);
+            if (result.Success == false)
+            {
+                return BadRequest(result);
+            }
+            return Ok(result);
+        }
+    }
+}
