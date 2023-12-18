016a2ab364f7dfa1a52df9d2ffd61d6a01984e9e
khanhndxd <khanhprox25x@gmail.com>
2023-12-18 15:26:10 +0700

    Add RepairLog table for order's history

---
+++ Models/RepairLog.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace repair_management_backend.Models
+{
+    public class RepairLog
+    {
+        public int Id { get; set; }
+        public int RepairOrderId { get; set; }
+        [ForeignKey("CreatedBy")]
+        public string CreatedById { get; set; }
+        public virtual User CreatedBy { get; set; }
+        public DateTime CreatedAt { get; set; }
+        [MaxLength(255)]
+        public string Info { get; set; }
+    }
+}
