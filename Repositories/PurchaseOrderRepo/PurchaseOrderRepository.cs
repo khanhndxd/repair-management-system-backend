f30b1865eed47dd80484cc2668782a977b8b607f
khanhndxd <khanhprox25x@gmail.com>
2023-12-18 15:39:31 +0700

    Update PuchasedOrder services

--- Repositories/PurchaseOrderRepo/PurchaseOrderRepository.cs
+++ Repositories/PurchaseOrderRepo/PurchaseOrderRepository.cs
@@ -1,4 +1,5 @@
 ﻿using AutoMapper;
+using repair_management_backend.DTOs.PurchasedProduct;
 using repair_management_backend.DTOs.PurchaseOrder;
 
 namespace repair_management_backend.Repositories.PurchaseOrderRepo
@@ -17,14 +18,63 @@ namespace repair_management_backend.Repositories.PurchaseOrderRepo
             var serviceResponse = new ServiceResponse<List<GetPurchaseOrderDTO>>();
             try
             {
-                var result = await  _dataContext.PurchaseOrders.Include(x => x.Customer).Include(x => x.PurchaseProducts).Where(x => x.CustomerId == id).Select(x => _mapper.Map<GetPurchaseOrderDTO>(x)).ToListAsync();
-                if(result.Count == 0)
+                var result = await _dataContext.PurchaseOrders
+                    .Include(x => x.Customer)
+                    .Include(x => x.PurchaseProducts)
+                        .ThenInclude(x => x.Category).ThenInclude(x => x.WarrantyPolicy)
+                    .Where(x => x.CustomerId == id)
+                    .ToListAsync();
+
+                if (result.Count == 0)
                 {
                     throw new Exception($"Không tìm thấy đơn sửa chữa bảo dưỡng nào của khách hàng có id là `{id}`");
                 }
-                serviceResponse.Data = result;
 
-            } catch(Exception ex)
+                // Lấy danh sách tên sản phẩm từ tất cả các đơn hàng
+                var purchasedProductNames = result.SelectMany(po => po.PurchaseProducts.Select(pp => pp.ProductName)).ToList();
+
+                // Truy vấn bảng Accessory để lấy giá gốc
+                var accessoryPrices = await _dataContext.Accessories
+                    .Where(a => purchasedProductNames.Contains(a.Name))
+                    .ToDictionaryAsync(a => a.Name, a => a.Price);
+
+                // Tạo danh sách DTO từ danh sách đơn hàng
+                var dtos = result.Select(po =>
+                {
+                    var purchaseProducts = po.PurchaseProducts.Select(pp =>
+                    {
+                        var purchasedProductDto = _mapper.Map<GetPurchasedProductDTO>(pp);
+                        var priceKeyValuePair = accessoryPrices.FirstOrDefault(kv =>
+                            string.Equals(kv.Key, pp.ProductName, StringComparison.OrdinalIgnoreCase));
+
+                        // Thêm trường giá gốc
+                        if (!string.IsNullOrEmpty(priceKeyValuePair.Key))
+                        {
+                            purchasedProductDto.OriginalPrice = priceKeyValuePair.Value;
+                        }
+                        else
+                        {
+                            // Xử lý trường hợp không tìm thấy giá gốc cho sản phẩm
+                            purchasedProductDto.OriginalPrice = 0;
+                        }
+                        // Kiểm tra trạng thái hết hạn bảo hành
+                        purchasedProductDto.IsWarrantyExpired = DateTime.UtcNow > pp.WarrantyPeriod;
+
+                        // Tính số ngày còn lại trong bảo hành
+                        purchasedProductDto.DaysLeftInWarranty = (int)(pp.WarrantyPeriod - DateTime.UtcNow).TotalDays;
+                        return purchasedProductDto;
+                    }).ToList();
+
+                    var dto = _mapper.Map<GetPurchaseOrderDTO>(po);
+                    // Thêm danh sách PurchaseProducts với giá gốc vào DTO
+                    dto.PurchaseProducts = purchaseProducts;
+
+                    return dto;
+                }).ToList();
+
+                serviceResponse.Data = dtos;
+            }
+            catch (Exception ex)
             {
                 serviceResponse.Success = false;
                 serviceResponse.Message = ex.Message;
