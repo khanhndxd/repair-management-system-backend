30afe03b0e141ea27a3d64e8e5237dc1fbe1a8a2
khanhndxd <khanhprox25x@gmail.com>
2023-12-18 15:21:13 +0700

    Update category dtos

--- DTOs/Category/GetCategoryDTO.cs
+++ DTOs/Category/GetCategoryDTO.cs
@@ -7,5 +7,6 @@ namespace repair_management_backend.DTOs.Category
         public int Id { get; set; }
         [MaxLength(50)]
         public string Name { get; set; }
+        public WarrantyPolicy WarrantyPolicy { get; set; }
     }
 }
