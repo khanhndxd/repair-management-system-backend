757eab99cf30557f44e88bc82041a8994a2da94a
khanhndxd <khanhprox25x@gmail.com>
2023-12-18 15:30:42 +0700

    Add WarrantyPolicyTask dtos

---
+++ DTOs/WarrantyPolicyTask/GetWarrantyPolicyTaskDTO.cs
@@ -0,0 +1,11 @@
+﻿using repair_management_backend.DTOs.Task;
+
+namespace repair_management_backend.DTOs.WarrantyPolicyTask
+{
+    public class GetWarrantyPolicyTaskDTO
+    {
+        public int Id { get; set; }
+        public WarrantyPolicy WarrantyPolicy { get; set; }
+        public GetTaskDTO Task { get; set; }
+    }
+}
