187d0705528ebef956741e79bac1741b3622ea8d
khanhndxd <khanhprox25x@gmail.com>
2023-12-18 15:22:28 +0700

    Update RepairLog dtos

---
+++ DTOs/RepairLog/AddRepairLogDTO.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace repair_management_backend.DTOs.RepairLog
+{
+    public class AddRepairLogDTO
+    {
+        public int RepairOrderId { get; set; }
+        public string CreatedById { get; set; }
+        public DateTime CreatedAt { get; set; }
+        [MaxLength(255)]
+        public string Info { get; set; }
+    }
+}
---
+++ DTOs/RepairLog/GetRepairLogDTO.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using repair_management_backend.DTOs.User;
+
+namespace repair_management_backend.DTOs.RepairLog
+{
+    public class GetRepairLogDTO
+    {
+        public int Id { get; set; }
+        public int RepairOrderId { get; set; }
+        public virtual GetUserDTO CreatedBy { get; set; }
+        public DateTime CreatedAt { get; set; }
+        [MaxLength(255)]
+        public string Info { get; set; }
+    }
+}
