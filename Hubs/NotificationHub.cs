5b5735cccd2c3fdd1cd92720fceddbe212bd6674
khanhndxd <khanhprox25x@gmail.com>
2023-12-18 15:20:11 +0700

    Add Notification Hub in SignalR

---
+++ Hubs/NotificationHub.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.SignalR;
+
+namespace repair_management_backend.Hubs
+{
+    //[Authorize(Policy = "ReadWritePolicy")]
+    public class NotificationHub : Hub
+    {
+        public override async System.Threading.Tasks.Task OnConnectedAsync()
+        {
+            await base.OnConnectedAsync();
+        }
+        public async System.Threading.Tasks.Task JoinGroup(int orderId)
+        {
+            await Groups.AddToGroupAsync(Context.ConnectionId, $"order-{orderId}"); 
+        }
+
+        public async System.Threading.Tasks.Task LeaveGroup(int orderId)
+        {
+            await Groups.RemoveFromGroupAsync(Context.ConnectionId, $"order-{orderId}");
+        }
+    }
+}
