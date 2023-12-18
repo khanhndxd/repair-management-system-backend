529a90992e9b7623fc2d1296c5f27ef4cc19be9e
khanhndxd <khanhprox25x@gmail.com>
2023-12-18 15:25:05 +0700

    Add WarrantyPolicy and WarrantyPolicyTask tables for applying warranty policy service

---
+++ Models/WarrantyPolicy.cs
@@ -0,0 +1,9 @@
+﻿namespace repair_management_backend.Models
+{
+    public class WarrantyPolicy
+    {
+        public int Id { get; set; }
+        public int CategoryId { get; set; }
+        public string Description { get; set; }
+    }
+}
---
+++ Models/WarrantyPolicyTask.cs
@@ -0,0 +1,13 @@
+﻿namespace repair_management_backend.Models
+{
+    public class WarrantyPolicyTask
+    {
+        public int Id { get; set; }
+        public int WarrantyPolicyId { get; set; }
+        public int TaskId { get; set; }
+        [DeleteBehavior(DeleteBehavior.NoAction)]
+        public WarrantyPolicy WarrantyPolicy { get; set; }
+        [DeleteBehavior(DeleteBehavior.NoAction)]
+        public Task Task { get; set; }
+    }
+}
